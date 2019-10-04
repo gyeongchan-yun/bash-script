3110307de7570df711625719a0a94e00cee7d8ca
Gyeongchan Yun <rugyoon@gmail.com>
2019-10-04 19:25:57 +0900

    ADD script for asking input yes or no

---
+++ yes_no_input.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+file=""
+
+if [ -f $file ] ; then
+    read -p "[CAUTION] .tmux.conf already exists. Do you want to overwrite [Y/n]? " yn
+    case $yn in
+    ┆    "")
+              # do something 
+    ┆    ┆    ;;
+    ┆    [Yy]*)
+              # do something 
+    ┆    ┆    ;;
+    ┆    [Nn]*)
+              # do something 
+    ┆    ┆    ;;
+    esac
+fi
